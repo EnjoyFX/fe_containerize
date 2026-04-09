6b0e20f2d7f9c1579191486196c6b264853b736d
Andrii <vba2you@gmail.com>
2026-04-09 23:47:10 +0300

    Disable X-Powered-By header to hide framework info

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.getHttpAdapter().getInstance().disable('x-powered-by');
   app.setGlobalPrefix('api');
 
   app.enableCors({
