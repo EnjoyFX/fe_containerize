cf5c8ae93068262ff8b13a39e87507e6938e37c1
Andrii <vba2you@gmail.com>
2026-04-11 13:45:28 +0300

    Fix JSON import in vite config for Node compatibility

--- microfrontend/vite.config.js
+++ microfrontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import pkg from './package.json' with { type: 'json' }
+import { readFileSync } from 'fs'
+
+const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'))
 
 export default defineConfig({
   plugins: [react()],
